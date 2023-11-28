234e8940d948ebde742ae590650a83ff0487ad1b
Rami-Agha <ramiagha888@gmail.com>
2023-11-28 13:00:22 +0100

    html and css all elements added and styles/placing

--- mickeyemail/src/App.jsx
+++ mickeyemail/src/App.jsx
@@ -24,71 +24,36 @@ function App() {
     <>
       <div className="wrapper">
         <section className="grid-containera-info">
-          <div className="input">
+          <div className="inputs">
             <input type="text" placeholder="From:" className="from" />
             <input type="text" placeholder="To:" className="to" />
             <input type="text" placeholder="Latest:" className="latest" />
-            <div classname="notification">
+
+            <div id="notification">
               <p className="amount">Notification amount</p>
-            {/* test on increment add 1 */}
 
-            {count}
+              {/* test on increment add 1 */}
 
-            
-              <div onClick={OnIncrementClick} className="Button">
-                <button>+</button>
-              </div>
-              <div onClick={OnDecrementClick} className="Button">
-                <button>-</button>
-              </div>
-              {/* <p className="complete">
-              Completed
-              <input type="checkbox" />
-            </p> */}
-              <input type="text" placeholder="Context:" className="context" />
+              <div id="count">{count}</div>
+
+              <button className="button-amount1" onClick={OnIncrementClick}>
+                +
+              </button>
+
+              <button className="button-amount2" onClick={OnDecrementClick}>
+                -
+              </button>
+
             </div>
+            <input type="text" placeholder="Context:" className="context" />
+
           </div>
         </section>
 
-        <section className="grid-callender">
-          <div className="callender">
-            <h2>Date of return</h2>
-           <input type="datetime-local" id="tiddatum"/>
-            <table>
-              <thead>
-                <tr>
-                  <th>Sun</th>
-                  <th>Mon</th>
-                  <th>Tue</th>
-                  <th>Wed</th>
-                  <th>Thu</th>
-                  <th>Fri</th>
-                  <th>Sat</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                  <td>4</td>
-                </tr>
-                <tr>
-                  <td>5</td>
-                  <td>6</td>
-                  <td>7</td>
-                  <td>8</td>
-                  <td>9</td>
-                  <td>10</td>
-                  <td>11</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </section>
+        <div className="callender">
+          <h2>Date of return</h2>
+          <input type="datetime-local" id="tiddatum" />
+        </div>
       </div>
     </>
   );
