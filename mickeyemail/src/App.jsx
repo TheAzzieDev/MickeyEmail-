434b59b6f7f7c3b6e46acd7c5cf57c537f779a70
Rami-Agha <ramiagha888@gmail.com>
2023-11-29 09:32:21 +0100

    finnished basic structure for frontend
    added all of the elemnts needed for this project

--- mickeyemail/src/App.jsx
+++ mickeyemail/src/App.jsx
@@ -4,21 +4,21 @@ import "./App.css";
 import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  const OnIncrementClick = useCallback(
-    (e) => {
-      setCount(count + 1);
-    },
-    [count]
-  );
+  // const OnIncrementClick = useCallback(
+  //   (e) => {
+  //     setCount(count + 1);
+  //   },
+  //   [count]
+  // );
 
-  const OnDecrementClick = useCallback(
-    (e) => {
-      setCount(count - 1);
-    },
-    [count]
-  );
+  // const OnDecrementClick = useCallback(
+  //   (e) => {
+  //     setCount(count - 1);
+  //   },
+  //   [count]
+  // );
 
   return (
     <>
@@ -29,10 +29,10 @@ function App() {
             <input type="text" placeholder="To:" className="to" />
             <input type="text" placeholder="Subject:" className="subject" />
 
-            <div id="notification">
+            {/* <div id="notification">
               <p className="amount">Notification amount</p>
 
-              {/* test on increment add 1 */}
+              test on increment add 1
 
               <div id="count">{count}</div>
 
@@ -43,19 +43,42 @@ function App() {
               <button className="button-amount2" onClick={OnDecrementClick}>
                 -
               </button>
-            </div>
+            </div> */}
             <textarea placeholder="Context:" className="context" />
-
           </div>
         </section>
 
         <button id="send">Send</button>
 
+        <wrapper id="upload">
+          <form action="/upload" method="post" enctype="multipart/form-data">
+            {/* <label for="fileInput">Choose a file:</label> */}
+            <input
+              type="file"
+              id="fileInput"
+              name="fileInput"
+              accept=".jpg, .jpeg, .png, .gif, pdf"
+            ></input>
+            {/* <button type="submit" className="upload-btn">Upload</button> */}
+          </form>
+        </wrapper>
+
         <div className="callender">
           <h2>Date of return</h2>
           <input type="datetime-local" id="tiddatum" />
         </div>
-        <button onClick={() => {}}>Send To Recipiecent</button>
+
+        <wrapper id="datalist">
+          <label for="browser">Choose your finnished tasks?:</label>
+          <input list="browsers" name="browser" id="browser"></input>
+          <datalist id="browsers">
+            <option value="Edge" />
+            <option value="Firefox" />
+            <option value="Chrome" />
+            <option value="Opera" />
+            <option value="Safari" />
+          </datalist>
+        </wrapper>
       </div>
     </>
   );
