842a2aca20d32eb47d26315f4dca533255dd155d
zimonjohnsson <zimonjohnsson2004@gmail.com>
2023-11-30 14:14:26 +0100

    store date and time in localstorage

--- mickeyemail/src/App.jsx
+++ mickeyemail/src/App.jsx
@@ -1,121 +1,118 @@
 import React, { useState, useEffect } from "react";
-
 import "./App.css";
 import "./index.css";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
-  // const OnIncrementClick = useCallback(
-  //   (e) => {
-  //     setCount(count + 1);
-  //   },
-  //   [count]
-  // );
-
-  // const OnDecrementClick = useCallback(
-  //   (e) => {
-  //     setCount(count - 1);
-  //   },
-  //   [count]
-  // );
-
-  const [inputData, setInputData] = useState ('');
+  const [inputData, setInputData] = useState('');
   const [inputData2, setInputData2] = useState('');
-
-  const [dataList, setDataList] = useState (()=>{
-    const storedData=localStorage.getItem('dataList');
-    return storedData ? JSON.parse(storedData): [];
+  const [selectedDateTime, setSelectedDateTime] = useState('');
+  const [dataList, setDataList] = useState(() => {
+    const storedData = localStorage.getItem('dataList');
+    return storedData ? JSON.parse(storedData) : [];
   });
 
-  const handleInputChange=(event) => {
+  const handleInputChange = (event) => {
     setInputData(event.target.value);
   };
 
-  const handleInputChange2=(event) => {
+  const handleInputChange2 = (event) => {
     setInputData2(event.target.value);
   };
 
-  const handleButtonClick = () => {
-    const combinedData = `${inputData} - ${inputData2}`;
-    setDataList((prevList) => [...prevList, combinedData]);
+  const handleDateTimeChange = (event) => {
+    setSelectedDateTime(event.target.value);
+  };
 
+  const handleButtonClick = () => {
+    const selectedDate = new Date(selectedDateTime);
+
+    const year = selectedDate.getFullYear();
+    const month = selectedDate.getMonth() + 1; 
+    const day = selectedDate.getDate();
+    const hour = selectedDate.getHours();
+    const minute = selectedDate.getMinutes();
+
+    const combinedData = `${inputData} - ${inputData2} - ${year}:${month}:${day}-${hour}:${minute}`;
+
+    setDataList((prevList) => [...prevList, combinedData]);
     setInputData('');
     setInputData2('');
-  }
+    setSelectedDateTime('');
+  };
 
-  const handleCheckboxClick=(index)=>{
-    setDataList((prevList)=>{
-      const updatedList=[...prevList];
-      updatedList.splice(index,1);
+  const handleCheckboxClick = (index) => {
+    setDataList((prevList) => {
+      const updatedList = [...prevList];
+      updatedList.splice(index, 1);
       return updatedList;
     });
   };
 
-  useEffect (()=>{
+  useEffect(() => {
     localStorage.setItem('dataList', JSON.stringify(dataList));
-  },[dataList]);
- 
+  }, [dataList]);
+
   return (
     <>
       <div className="wrapper">
         <section className="grid-containera-info">
           <div className="inputs">
             <input type="text" placeholder="From:" className="from" />
-            <input type="text" placeholder="To:" className="to" value={inputData} onChange={handleInputChange}/>
-            <input type="text" placeholder="Subject:" className="subject" value={inputData2} onChange={handleInputChange2} />
-
-            {/* <div id="notification">
-              <p className="amount">Notification amount</p>
-
-              test on increment add 1
-
-              <div id="count">{count}</div>
-
-              <button className="button-amount1" onClick={OnIncrementClick}>
-                +
-              </button>
-
-              <button className="button-amount2" onClick={OnDecrementClick}>
-                -
-              </button>
-            </div> */}
+            <input
+              type="text"
+              placeholder="To:"
+              className="to"
+              value={inputData}
+              onChange={handleInputChange}
+            />
+            <input
+              type="text"
+              placeholder="Subject:"
+              className="subject"
+              value={inputData2}
+              onChange={handleInputChange2}
+            />
             <textarea placeholder="Context:" className="context" />
           </div>
 
-        <wrapper>
-          <button id="send" onClick={handleButtonClick}>Send</button>
-        </wrapper>
-        
-        <wrapper id="upload">
-          <form action="/upload" method="post" enctype="multipart/form-data">
-            {/* <label for="fileInput">Choose a file:</label> */}
-            <input
-              type="file"
-              id="fileInput"
-              name="fileInput"
-              accept=".jpg, .jpeg, .png, .gif, pdf"
-            ></input>
-            {/* <button type="submit" className="upload-btn">Upload</button> */}
-          </form>
-        </wrapper>
+          <div>
+            <button id="send" onClick={handleButtonClick}>
+              Send
+            </button>
+          </div>
+
+          <div id="upload">
+            <form action="/upload" method="post" enctype="multipart/form-data">
+              <input
+                type="file"
+                id="fileInput"
+                name="fileInput"
+                accept=".jpg, .jpeg, .png, .gif, pdf"
+              ></input>
+            </form>
+          </div>
         </section>
 
-        
         <div className="callender">
           <h2>Date of return</h2>
-          <input type="datetime-local" id="tiddatum" />
+          <input
+            type="datetime-local"
+            id="tiddatum"
+            value={selectedDateTime}
+            onChange={handleDateTimeChange}
+          />
         </div>
 
-        <wrapper id="datalist">
+        <div id="datalist">
           <h3>Open tasks:</h3>
           <ul>
-            {dataList.map((item,index) =>(
-              <li key="index">{item} <input type="checkbox" onChange={()=>handleCheckboxClick(index)}></input></li>
+            {dataList.map((item, index) => (
+              <li key={index}>
+                {item} <input type="checkbox" onChange={() => handleCheckboxClick(index)}></input>
+              </li>
             ))}
           </ul>
-        </wrapper>
-
+        </div>
       </div>
     </>
   );
